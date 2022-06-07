3b192a6924ae32dde7e26616c9d15487fb50db1e
Myles <bracken.myles@gmail.com>
2022-06-06 20:48:16 -0700

    updated answer and mess around with API fetch

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function fetchBooksSecond() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderFifth(json));
+  // To pass the tests, don't forget to return your fetch!
+}
+
+function renderFifth(list) {
+  const main = document.querySelector("main");
+  const fifth = list[4];
+  const h1 = document.createElement("h1");
+  h1.innerHTML = fifth.name;
+  main.appendChild(h1);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
+  fetchBooksSecond();
 });
